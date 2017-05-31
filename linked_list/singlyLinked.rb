d56b7c53c36467a57ae3ce1059d72ec7f98c86ab
Will Wilson <williamrwilson@gmail.com>
2017-05-30 22:10:07 -0400

    basic linked list

---
+++ linked_list/singlyLinked.rb
@@ -0,0 +1,140 @@
+require 'pp'
+
+class LinkedList
+
+  attr_accessor :headNode, :length
+
+  def initialize(data = nil)
+    if data != nil
+      @headNode = Node.new(data)
+    else
+      @headNode = nil
+    end
+  end
+
+  def append(data) #add to end of list
+    currentNode = @headNode
+    if currentNode.nil?
+      @headNode = Node.new(data)
+    else
+      until currentNode.nextNode.nil?
+        currentNode = currentNode.nextNode
+      end
+      currentNode.nextNode = Node.new(data)
+    end
+  end
+
+  def prepend(data) #add to front of list
+    if @headNode.nil?
+      @headNode = Node.new(data)
+    else
+      newHead = @headNode.clone
+      @headNode = Node.new(data)
+      @headNode.nextNode = newHead
+    end
+  end
+
+  def size
+    currentNode = @headNode
+    counter = 0
+    if @headNode.nil?
+      counter = 0
+    else
+      until currentNode.nil?
+        currentNode = currentNode.nextNode
+        counter += 1
+      end
+    end
+    return counter
+  end
+
+  def empty? #this assumes that @headNode hasn't been deleted, leaving the other nodes
+    @headNode.nil?
+  end
+
+  def head
+    if @headNode.nil?
+      ""
+    else
+      @headNode.data
+    end
+  end
+
+  def tail
+    currentNode = @headNode
+    until currentNode.nextNode.nil?
+      currentNode = currentNode.nextNode
+    end
+    currentNode.data
+  end
+
+  def at(index)
+    if (self.size - 1) < index
+      return "Error!  Index out of range"
+    else
+      currentNode = @headNode
+      counter = 0
+      until counter == index
+        currentNode = currentNode.nextNode
+        counter += 1
+      end
+      return currentNode.data
+    end
+  end
+
+  def pop
+    #can't use tail because I need next to last as well as last
+    penultimate = @headNode
+    ultimate = penultimate.nextNode
+    until ultimate.nextNode.nil?
+      penultimate = ultimate
+      ultimate = penultimate.nextNode
+    end
+    penultimate.nextNode = nil
+    returnVal = ultimate.data
+    ultimate = nil
+    return returnVal
+  end
+
+  def print_each
+    currentNode = @headNode
+    until currentNode.nil?
+      currentNode.display
+      currentNode = currentNode.nextNode
+    end
+  end
+
+end
+
+
+class Node
+
+  attr_accessor :data, :nextNode
+
+  def initialize(data)
+    @data = data
+    @nextNode = nil
+  end
+
+  def display
+    puts @data.to_s
+  end
+
+end
+
+l = LinkedList.new("last")
+l.prepend("seconde")
+l.prepend("first!")
+l.print_each
+puts "l.size: " + l.size.to_s
+puts "l.head: " + l.head.to_s
+puts "l.tail: " + l.tail
+puts "at index 3: #{l.at(3)}"
+puts "at index 2: #{l.at 2}"
+puts "at index 0: #{l.at 0}"
+
+puts l.pop
+
+l.print_each
+puts l.size
+puts "*************************************"
